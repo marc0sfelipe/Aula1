37980c710218b195a11930d8dbe3f2d225f2cb55
marc0sfelipe <marcos12004@hotmail.com>
2023-01-16 16:14:50 -0300

    first commit

---
+++ aula1.py
@@ -0,0 +1,12 @@
+nome = 'marcos'
+sobrenome = 'felipe'
+idade = 22
+e_maior_de_idade = idade >= 18
+altura = '1.69'
+
+
+print ('Nome:', nome)
+print ('Sobrenome:', sobrenome)
+print ('Idade:', idade)
+print ('É maior de idade?', e_maior_de_idade)
+print ('Altura em metros', altura)
